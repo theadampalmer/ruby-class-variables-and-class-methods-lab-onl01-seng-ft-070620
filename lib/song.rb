4fa0ac98c7b1d073d87dba40b9588366d57124b4
Adam Palmer <theadampalmer@gmail.com>
2020-07-26 23:37:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,13 +29,13 @@ require 'pry'
      end
     
      def self.genre_count
-       genre_count = {}
-       @@genres.each do |genre|
-           if genre_count[genre]
-           genre_count[genre] += 1
-           else
-           genre_count[genre] = 1
-           end
+      # genre_count = {}
+      # @@genres.each do |genre|
+      #     if genre_count[genre]
+      #     genre_count[genre] += 1
+      #     else
+      #     genre_count[genre] = 1
+      #     end
      end
     
        genre_count
